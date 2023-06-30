fa68458350deb5e90e6ec84c7e626f23be97b225
loukya-7 <131159563+loukya-7@users.noreply.github.com>
2023-06-30 20:15:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function updateOutputText() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+// Assuming you have an HTML element with the ID "output"
+const outputElement = document.getElementById("output");
+
+updateOutputText().then((message) => {
+  outputElement.innerText = message;
+})
